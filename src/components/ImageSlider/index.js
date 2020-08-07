f1767564f32bef3443b8c5797659dc271a825632
Scott M Massey <scottmmassey@MacBook-Pro.attlocal.net>
2020-08-07 15:51:56 -0500

    added arrows

--- src/components/ImageSlider/index.js
+++ src/components/ImageSlider/index.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react'
-import { Platform, Image, ScrollView, Text, View, TouchableOpacity, TouchableWithoutFeedback, StyleSheet } from 'react-native'
+import {
+  Platform,
+  Image,
+  ScrollView,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const HIT_SLOP = {
   top: 4,
@@ -12,8 +22,41 @@ class ImageItem extends Component {
   render() {
     let { image } = this.props
 
+    return <Image style={styles.image} resizeMode="cover" source={image} />
+  }
+}
+
+class Arrow extends Component {
+  render() {
+    const {
+      centerArrows,
+      name,
+      color,
+      enableBackground,
+      backgroundColor,
+      backgroundRounding,
+      enableBorder,
+      borderSize,
+      borderColor,
+      size,
+      onPress,
+    } = this.props
+    const viewHeight =
+      size + (enableBackground && enableBorder ? borderSize : 0)
+    const viewStyle = {
+      backgroundColor: enableBackground ? backgroundColor : null,
+      borderRadius: enableBackground ? backgroundRounding : null,
+      borderColor: enableBorder && enableBackground ? borderColor : null,
+      borderWidth: enableBorder && enableBackground ? borderSize : null,
+      height: viewHeight,
+      alignSelf: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
     return (
-      <Image style={styles.image} resizeMode="cover" source={image} />
+      <View style={viewStyle}>
+        <Icon name={name} color={color} size={size} onPress={onPress} />
+      </View>
     )
   }
 }
@@ -23,7 +66,7 @@ class Images extends Component {
     activeIndex: 0,
   }
 
-  calculateIndex = offsetX => {
+  calculateIndex = (offsetX) => {
     let { containerWidth } = this.props
     let index = Math.round(offsetX / containerWidth)
 
@@ -36,12 +79,12 @@ class Images extends Component {
     this.currentOffset = x
     let index = this.calculateIndex(x)
 
-    if (index !== activeIndex) {  
+    if (index !== activeIndex) {
       this.setState({ activeIndex: index })
     }
   }
 
-  handleChange = index => {
+  handleChange = (index) => {
     let { containerWidth, images } = this.props
     let offset = Math.min(images.length - 1, index) * containerWidth
 
@@ -50,13 +93,27 @@ class Images extends Component {
     this.setState({ activeIndex: index })
   }
 
-  handlePress = i => () => {
+  handleRightArrow = () => {
+    const { activeIndex } = this.state
+    const { images } = this.props
+    if (activeIndex > images.length - 2) return
+    this.handleChange(activeIndex + 1)
+  }
+
+  handleLeftArrow = () => {
+    console.log('handleLeftArrow')
+    const { activeIndex } = this.state
+    if (activeIndex === 0) return
+    this.handleChange(activeIndex - 1)
+  }
+
+  handlePress = (i) => () => {
     let { images } = this.props
 
     images[i] && images[i].action && images[i].action()
   }
 
-  scrollViewRef = el => {
+  scrollViewRef = (el) => {
     this.scrollView = el
   }
 
@@ -70,6 +127,7 @@ class Images extends Component {
       editor,
       paddingBottom,
       dotsEnabled,
+      arrows,
     } = this.props
 
     let { activeIndex } = this.state
@@ -84,43 +142,126 @@ class Images extends Component {
 
     let scrollViewStyles = { paddingBottom }
 
+    const {
+      enabled: enableArrows,
+      leftIcon,
+      rightIcon,
+      iconColor,
+      enableBackground,
+      backgroundColor,
+      backgroundRounding,
+      enableBorder,
+      borderSize,
+      borderColor,
+      iconSize,
+    } = arrows
+    const centerArrows = true
+    const arrowsInScroll = centerArrows || paddingBottom === 0
     return (
-      <View style={[styles.scrollViewWrapper, scrollViewStyles]}>
-        {editor ? (
-          <View style={styles.placeholder} />
-        ) : (
-          <ScrollView
-            horizontal
-            pagingEnabled
-            style={[styles.scrollView]}
-            ref={this.scrollViewRef}
-            onScroll={this.handleScroll}
-            scrollEventThrottle={0}
-            showsHorizontalScrollIndicator={false}
-          >
-            <View style={[styles.imageContainer, wrapperStyles]}>
-              {!editor && images.map(({ id, image }, i) => (
-                <TouchableWithoutFeedback onPress={this.handlePress(i)}>
-                  <View style={[styles.imageWrapper, innerWrapper]}>
-                    <ImageItem
-                      key={id}
-                      image={image}
-                    />
-                  </View>
-                </TouchableWithoutFeedback>
-              ))}
-            </View>
-          </ScrollView>
-        )}
-        {dotsEnabled && (
-          <Dots
-            count={images.length}
-            activeIndex={activeIndex}
-            activeColor={activeColor}
-            inactiveColor={inactiveColor}
-            onChange={this.handleChange}
-          />
-        )}
+      <View style={[styles.scrollViewWrapper]}>
+        <View
+          style={[
+            styles.arrowScrollWrapper,
+            scrollViewStyles,
+            styles.scrollViewWrapper,
+          ]}
+        >
+          {enableArrows && arrowsInScroll && (
+            <Arrow
+              centerArrows={centerArrows}
+              name={leftIcon}
+              color={iconColor}
+              enableBackground={enableBackground}
+              backgroundColor={backgroundColor}
+              backgroundRounding={backgroundRounding}
+              enableBorder={enableBorder}
+              borderSize={borderSize}
+              borderColor={borderColor}
+              size={iconSize}
+              onPress={this.handleLeftArrow}
+            />
+          )}
+          {editor ? (
+            <View style={styles.placeholder} />
+          ) : (
+            <ScrollView
+              horizontal
+              pagingEnabled
+              style={[styles.scrollView]}
+              ref={this.scrollViewRef}
+              onScroll={this.handleScroll}
+              scrollEventThrottle={0}
+              showsHorizontalScrollIndicator={false}
+            >
+              <View style={[styles.imageContainer, wrapperStyles]}>
+                {!editor &&
+                  images.map(({ id, image }, i) => (
+                    <TouchableWithoutFeedback onPress={this.handlePress(i)}>
+                      <View style={[styles.imageWrapper, innerWrapper]}>
+                        <ImageItem key={id} image={image} />
+                      </View>
+                    </TouchableWithoutFeedback>
+                  ))}
+              </View>
+            </ScrollView>
+          )}
+          {enableArrows && arrowsInScroll && (
+            <Arrow
+              centerArrows={centerArrows}
+              name={rightIcon}
+              color={iconColor}
+              enableBackground={enableBackground}
+              backgroundColor={backgroundColor}
+              backgroundRounding={backgroundRounding}
+              enableBorder={enableBorder}
+              borderSize={borderSize}
+              borderColor={borderColor}
+              size={iconSize}
+              onPress={this.handleRightArrow}
+            />
+          )}
+        </View>
+        <View style={styles.arrowDotsWrapper}>
+          {enableArrows && !arrowsInScroll && (
+            <Arrow
+              centerArrows={centerArrows}
+              name={leftIcon}
+              color={iconColor}
+              enableBackground={enableBackground}
+              backgroundColor={backgroundColor}
+              backgroundRounding={backgroundRounding}
+              enableBorder={enableBorder}
+              borderSize={borderSize}
+              borderColor={borderColor}
+              size={iconSize}
+              onPress={this.handleLeftArrow}
+            />
+          )}
+          {dotsEnabled && (
+            <Dots
+              count={images.length}
+              activeIndex={activeIndex}
+              activeColor={activeColor}
+              inactiveColor={inactiveColor}
+              onChange={this.handleChange}
+            />
+          )}
+          {enableArrows && !arrowsInScroll && (
+            <Arrow
+              centerArrows={centerArrows}
+              name={rightIcon}
+              color={iconColor}
+              enableBackground={enableBackground}
+              backgroundColor={backgroundColor}
+              backgroundRounding={backgroundRounding}
+              enableBorder={enableBorder}
+              borderSize={borderSize}
+              borderColor={borderColor}
+              size={iconSize}
+              onPress={this.handleRightArrow}
+            />
+          )}
+        </View>
       </View>
     )
   }
@@ -138,7 +279,7 @@ class Dots extends Component {
     return data
   }
 
-  handlePress = index => () => {
+  handlePress = (index) => () => {
     let { onChange } = this.props
 
     onChange(index)
@@ -155,7 +296,7 @@ class Dots extends Component {
             <View
               style={[
                 styles.dot,
-                { backgroundColor: active ? activeColor : inactiveColor }
+                { backgroundColor: active ? activeColor : inactiveColor },
               ]}
             />
           </TouchableOpacity>
@@ -186,7 +327,7 @@ class ImageSlider extends Component {
 
     if (width !== prevWidth || height !== prevHeight) {
       this.setState({ width, height })
-    }   
+    }
   }
 
   getDimensions() {
@@ -201,13 +342,20 @@ class ImageSlider extends Component {
 
   render() {
     const { width, height } = this.getDimensions()
-    const { images, dots, editor } = this.props
+    const { images, dots, editor, arrows } = this.props
 
     if (width === null || height === null) {
       return <View style={styles.wrapper} onLayout={this.handleLayout} />
     }
 
     let dotsEnabled = dots.enabled
+
+    let paddingBottom = !dotsEnabled || dots.position === 'inside' ? 0 : 40
+    /*paddingBottom =
+      !arrows.centerArrows && dotsEnabled && dots.position === 'outside'
+        ? 20
+        : paddingBottom
+    */
 
     return (
       <View style={styles.wrapper} onLayout={this.handleLayout}>
@@ -217,9 +365,10 @@ class ImageSlider extends Component {
           containerWidth={width}
           containerHeight={height}
           dotsEnabled={dotsEnabled}
-          paddingBottom={(!dotsEnabled || dots.position === 'inside') ? 0 : 40}
+          paddingBottom={paddingBottom}
           activeColor={dots.activeColor}
           inactiveColor={dots.inactiveColor}
+          arrows={arrows}
         />
       </View>
     )
@@ -257,8 +406,8 @@ const styles = StyleSheet.create({
     flex: 1,
     ...Platform.select({
       web: {
-        scrollSnapType: 'x mandatory'
-      }
+        scrollSnapType: 'x mandatory',
+      },
     }),
   },
   imageContainer: {
@@ -281,6 +430,13 @@ const styles = StyleSheet.create({
   placeholder: {
     flex: 1,
     backgroundColor: '#eee',
+  },
+  arrowScrollWrapper: {
+    flexDirection: 'row',
+  },
+  arrowDotsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 })
 
